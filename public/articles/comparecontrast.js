baad5fecb6a2dc2b320a949ca25f7f27d0ca1c22
ApalloAltoBluck <bluckstar1@gmail.com>
2020-12-09 22:17:22 -0500

    Revert "fixed it so index.html is outputted."
    This reverts commit ae90fd0e3c57dd3923f4a2416d142bc6e82415a0.

--- public/articles/comparecontrast.js
+++
@@ -1,8 +0,0 @@
-
-export const comparecontrast = {
-    title: "Batman: Gender and Class in Gotham City",
-    description: "This was completed for Northeastern's English writing class, taught by Dr. Tabitha Clark. Here, I examine the cultural differences between two entries in the Batman series, and focus on the divergences in class and gender.",
-    date: "November 12th",
-    markdown: "comparecontrast",
-    tag: "ENGW1111"
-}
--- static/articles/thesoloist.js
+++
@@ -1,8 +0,0 @@
-
-export const thesoloist = {
-    title: "Miyashita’s Poignant Tour-de-force Sweeps the Runway – Takahiromiyashita The Soloist FALL 2020 Menswear",
-    description: "This was completed for Northeastern's English writing class, taught by Dr. Tabitha Clark. Here, I review Takahiro Miyashita's FW 2020 collection.",
-    date: "November 12th",
-    markdown: "thesoloist",
-    tag: "ENGW1111"
-}
